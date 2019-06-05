c180bd20c8648781b1a18b846a9cab953ecf8a18
James Cuthbert <techie@jcuthbert.org.uk>
2019-06-05 16:42:20 +0100

    Database Schema

---
+++ AppoinmentCalendar.db/dbo/Tables/Appointment.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Appointment] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Summary]   VARCHAR (255) NOT NULL,
+    [Location]  VARCHAR (255) NOT NULL,
+    [StartDate] DATETIME      NOT NULL,
+    [EndDate]   DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
